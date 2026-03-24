29904326ecf0876024c83ee9bf90abb842beaa68
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2026-03-24 14:44:30 +0000

    ioujtrgiuhbtfrg

--- script.js
+++ script.js
@@ -3,5 +3,23 @@ const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
 taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI!!!")
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value;
+    li.textContent = taskInputValue; 
+    taskList.appendChild(li);
+    const span = document.createElement('span')
+    span.textContent = taskInputValue;
+    const button = document.createElement('button')
+    button.textContent = 'Excluir'
+    button.addEventListener('click', function(){
+        li.remove();
+    })
+
+
+    li.appendChild(span)
+    li.appendChild(button)
+
+    if (taskInputValue == ''){
+        return
+    }
 })
