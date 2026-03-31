194a61d718e8c7933d66e4aec42a2b116dcc1b95
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2026-03-31 14:26:12 +0000

    fgjhftjfytjfty

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const taskBtn = document.querySelector("#taskBtn")
 const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
+function addTask(){
+     const li = document.createElement('li');
+    const taskInputValue = taskInput.value; 
+    if (taskInputValue == ''){
+        return
+    }
+}
+
+function createDeleteButton(){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir'
+    button.addEventListener('click', function(){
+        li.remove();
+    })
+}
+
+
+function createSpan(taskInputValue){
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    return span;
+}
 
 taskBtn.addEventListener("click", function(){
     const li = document.createElement('li');
-    const taskInputValue = taskInput.value;
-    li.textContent = taskInputValue; 
-    taskList.appendChild(li);
-    const span = document.createElement('span')
-    span.textContent = taskInputValue;
-    const button = document.createElement('button')
+    const taskInputValue = taskInput.value; 
+    if (taskInputValue == ''){
+        return
+    }
+    
+    const button = document.createElement('button');
     button.textContent = 'Excluir'
     button.addEventListener('click', function(){
         li.remove();
     })
 
-
     li.appendChild(span)
     li.appendChild(button)
 
-    if (taskInputValue == ''){
-        return
-    }
-})
+    taskList.appendChild(li);
+}) 
+
+taskBtn.addEventListener("click", addTask)
